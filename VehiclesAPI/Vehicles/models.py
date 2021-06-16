82f9b222d21a68c5601b4f74bcc67e0b9659b9aa
Alejandro Morales <43051685+alemor10@users.noreply.github.com>
2021-06-15 22:00:17 -0700

    feat( add models with inheritance)

--- VehiclesAPI/Vehicles/models.py
+++ VehiclesAPI/Vehicles/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class BaseVehicle(models.Model):
+    Make = models.CharField(max_length=255)
+    Model = models.CharField(max_length=255)
+    Year = models.IntegerField()
+    ServiceInterval = models.CharField(max_length=255)
+    NextService = models.CharField(max_length=255)
+    
+    class Meta:
+        abstract = True
+        
+    def __str__(self):
+        return self.Make + '-' + self.Model
+        
+
+
+class Car( BaseVehicle ):
+    Seats = models.IntegerField()
+    Color = models.CharField(max_length=255)
+    VIN = models.CharField(max_length=255)
+    CurrentMileage = models.IntegerField()
+
+class Truck( BaseVehicle ):
+    Seats = models.IntegerField()
+    BedLength = models.CharField(max_length=255)
+    Color = models.CharField(max_length=255)
+    VIN = models.CharField(max_length=255)
+    CurrentMileage = models.IntegerField()
+
+class Boat( BaseVehicle ):
+    Length = models.IntegerField()
+    Width = models.CharField(max_length=255)
+    HIN = models.CharField(max_length=255)
+    CurrentHours = models.IntegerField()
--- VehiclesAPI/VehiclesAPI/settings.py
+++ VehiclesAPI/VehiclesAPI/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Vehicles'
+    'Vehicles.apps.VehiclesConfig'
 ]
 
 MIDDLEWARE = [
